4dd315c060aa365bc3d22e945fcf3edfa05d9776
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-09-10 00:20:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -39,7 +39,9 @@ end
 def now_serving(katz_deli)
   if katz_deli.count < 1
     puts "There is nobody waiting to be served!"
-  
+  elsif katz_deli.count > 1
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.
   end
 end  
    
